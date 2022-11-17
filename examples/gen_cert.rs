8902172c782d20afecef75b15ef9a0b6aa51ab7b
caozheng <caozheng@trolia.com>
2022-11-17 09:08:56 +0800

    gen cert

---
+++ examples/gen_cert.rs
@@ -0,0 +1,65 @@
+use anyhow::Result;
+use certify::{generate_ca, generate_cert, load_ca, CertType, CA};
+use tokio::fs;
+
+struct CertPem {
+    cert_type: CertType,
+    cert: String,
+    key: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let ca_pem = create_ca()?;
+    let ca = load_ca(&ca_pem.cert, &ca_pem.key)?;
+    let pem = create_cert(&ca, &["kvserver.acme.inc"], "Acme KV server", false)?;
+    gen_files(&pem).await?;
+    let pem = create_cert(&ca, &[], "awesome-device-id", true)?;
+    gen_files(&pem).await?;
+
+    Ok(())
+}
+
+fn create_ca() -> Result<CertPem> {
+    let (cert, key) = generate_ca(
+        &["acme.inc"],
+        "CN",
+        "Acme Inc.",
+        "Acme CA",
+        None,
+        Some(10 * 365),
+    )?;
+
+    Ok(CertPem {
+        cert_type: CertType::CA,
+        cert: cert,
+        key: key,
+    })
+}
+
+fn create_cert(ca: &CA, domains: &[&str], cn: &str, is_client: bool) -> Result<CertPem> {
+    let (days, cert_type) = if is_client {
+        (Some(365), CertType::Client)
+    } else {
+        (Some(365 * 3), CertType::Server)
+    };
+    let (cert, key) = generate_cert(ca, domains, "CN", "Acme Inc.", cn, None, is_client, days)?;
+
+    Ok(CertPem {
+        cert_type: cert_type,
+        cert: cert,
+        key: key,
+    })
+}
+
+async fn gen_files(pem: &CertPem) -> Result<()> {
+    let name = match pem.cert_type {
+        CertType::Client => "client",
+        CertType::Server => "server",
+        CertType::CA => "ca",
+    };
+    fs::write(format!("fixtures/{}.cert", name), pem.cert.as_bytes()).await?;
+    fs::write(format!("fixtures/{}.key", name), pem.key.as_bytes()).await?;
+
+    Ok(())
+}
